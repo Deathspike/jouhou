94ded280457afc0b3c4f3d58da3449e527eb7ff6
Deathspike <>
2012-09-28 12:01:23 +0200

    License information added to source files.

--- Jouhou/Abstract/IConnectionPool.cs
+++ Jouhou/Abstract/IConnectionPool.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿// ======================================================================
+// This source code form is subject to the terms of the Mozilla Public
+// License, version 2.0. If a copy of the MPL was not distributed with 
+// this file, you can obtain one at http://mozilla.org/MPL/2.0/.
+// ======================================================================
+using System;
 using System.Data.Common;
 using System.Threading.Tasks;
 
